948d651e54dad3db48548899ef63dc9b1b78ea97
Arnav@Victor <shubhamsaxena28282@gmail.com>
2025-03-03 16:18:08 +0530

    [Arnav] HomeControllerPUT

--- src/main/java/com/example/RestAPI/controller/HelloControllerPUT.java
+++ src/main/java/com/example/RestAPI/controller/HelloControllerPUT.java
@@ -1,4 +1,15 @@
 package com.example.RestAPI.controller;
 
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/hello")
 public class HelloControllerPUT {
+
+    // Handle PUT request with path variable and query parameter
+    @PutMapping("/put/{firstName}")
+    public String sayHello(@PathVariable String firstName, @RequestParam String lastName) {
+        return "Hello " + firstName + " " + lastName + " from BridgeLabz!!!!";
+        //curl.exe -X PUT "http://localhost:8080/hello/put/Arnav?lastName=Saxena" -w "\n"
+    }
 }
